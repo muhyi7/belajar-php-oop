887523b36ba235586740e186125cc6552a6ad61c
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-29 10:04:15 +0700

    Function

--- data/Person.php
+++ data/Person.php
@@ -5,4 +5,9 @@ class Person
     var string $name;
     var ?string $address = null;
     var string $country = "Indonesia";
+
+    function sayHello(string $name)
+    {
+      echo "Hello $name" . PHP_EOL;
+    }
 }
