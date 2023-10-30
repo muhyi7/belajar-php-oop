a317763fec72a0d98555ec9de959f0197bf3a74b
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-30 08:40:03 +0700

    Function Overriding, Parent Keyword

--- data/Manager.php
+++ data/Manager.php
@@ -5,10 +5,13 @@ class Manager{
 
     function sayHello(?string $name): void
     {
-        echo "Hi $name, my name is $this->name" . PHP_EOL;
+        echo "Hi $name, my name is Manager $this->name" . PHP_EOL;
     }
 }
 
 class VicePresident extends Manager {
-
+    function sayHello(?string $name): void
+    {
+        echo "Hi $name, my name is VP $this->name" . PHP_EOL;
+    }
 }
