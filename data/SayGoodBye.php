8dea5b4027a9adea4971809529045b73c8ecdf41
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-11 06:29:50 +0700

    Trait Conflict and Inheritence

--- data/SayGoodBye.php
+++ data/SayGoodBye.php
@@ -47,13 +47,18 @@ class ParentPerson{
     }
 }
 
-class Person extends ParentPerson
-{
+trait All{
     use SayGoodBye,SayHello, HasName, CanRun{
 //        bisa di override
 //        hello as private;
 //        goodBye as private;
     }
+}
+
+class Person extends ParentPerson
+{
+    use All;
+
     public function run(): void
     {
         echo "Person $this->name is running". PHP_EOL;
