d8146451222e6ad87d49d9ead37f3440fcde32be
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-01-13 21:37:06 +0700

    Generator

---
+++ Generator.php
@@ -0,0 +1,25 @@
+<?php
+
+function getGenap(int $max): Iterator
+{
+    $array = [];
+    for ($i = 1; $i <= $max; $i++) {
+        if ($i % 2 == 0) {
+            $array[] = $i;
+        }
+    }
+    return new ArrayIterator($array);
+}
+
+foreach (getGenap(100) as $value) {
+    echo "Genap : $value" . PHP_EOL;
+}
+
+function getGanjil(int $max): Iterator
+{
+    for ($i = i; $i <= $max; $i++) {
+        if ($i % 2 == 1) {
+            yield $i;
+        }
+    }
+}
